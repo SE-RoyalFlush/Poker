1cfd0f12a4b6bc276d79b97312400bbe5152f896
Sanikommu Devi <sanikommus3@gmail.com>
2026-02-17 18:26:13 -0500

    chore: added Angular initialization and global styles (#8)

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    MatToolbarModule, 
+    MatButtonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  title = 'RoyalFlush'; 
 }
